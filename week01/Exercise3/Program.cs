88c70f105ffd7017d0e511d04c83fd7aed6df960
angietdev <tatiana.rincon.guio@gmail.com>
2026-07-11 18:45:18 -0500

    ready excercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.Write ("What is the magic number? ");
+        //string userInput = Console.ReadLine();
+        //int magicNumber = int.Parse (userInput);
+
+        string response;
+
+        do
+        {
+        Random number = new Random ();
+        int magicNumber = number.Next (1,11);
+
+        Console.Write ("What is your guess? ");
+        string userInputGuess = Console.ReadLine();
+        int guess = int.Parse (userInputGuess);
+
+
+        while (guess != magicNumber)
+        {   
+        if (guess > magicNumber)
+        {
+            Console.WriteLine ("Lower");
+        }
+        else
+        {
+            Console.WriteLine ("Higher");
+        }
+        Console.Write ("What is your guess? ");
+        userInputGuess = Console.ReadLine();
+        guess = int.Parse (userInputGuess);
+        }
+        Console.WriteLine ("You guessed it!");
+
+        Console.Write ("Do you want to contunue? (yes/no)");
+        response =Console.ReadLine ();
+        }
+        while (response == "yes");
     }
+    
 }
