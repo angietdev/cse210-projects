8757fffe7eceae8f14114c7b2eb7511cf6102cce
angietdev <tatiana.rincon.guio@gmail.com>
2026-07-25 17:13:27 -0500

    week 3 assigment

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 
+using System;
+using System.Collections.Generic;
+
 class Program
 {
     static void Main(string[] args)
@@ -16,18 +19,18 @@ class Program
         List<Scripture> library = new List<Scripture>
         {
             new Scripture(
-                new Reference("John", 3, 16),
-                "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."
+                new Reference("Philippians", 4, 13),
+                "I can do all things through Christ which strengtheneth me."
             ),
 
             new Scripture(
-                new Reference("Proverbs", 3, 5, 6),
-                "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him, and he shall direct thy paths."
+                new Reference("Joshua", 1, 9),
+                "Have not I commanded thee? Be strong and of a good courage; be not afraid, neither be thou dismayed: for the Lord thy God is with thee whithersoever thou goest."
             ),
 
             new Scripture(
-                new Reference("Matthew", 5, 9),
-                "Blessed are the peacemakers, for they shall be called the children of God."
+                new Reference("Proverbs", 16, 3),
+                "Commit thy works unto the Lord, and thy thoughts shall be established."
             )
         };
 
