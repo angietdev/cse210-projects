3c7cd71d23cc17e17fcdc1da4456d51eefa4b590
angietdev <tatiana.rincon.guio@gmail.com>
2026-07-31 00:19:13 -0500

    adjust order week04

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,22 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Order 1 - USA Customer
-        Address address1 = new Address("47 Fleet St", "New York", "NY", "USA");
-        Customer customer1 = new Customer("Emmanuel Landon", address1);
+        // Order 1
+        Address address1 = new Address("875 Sunset Blvd", "Los Angeles", "CA", "USA");
+        Customer customer1 = new Customer("Olivia Carter", address1);
         Order order1 = new Order(customer1);
-        order1.AddProduct(new Product("Game pad", "A1012", 799.99, 1));
-        order1.AddProduct(new Product("Flash drive", "A1013", 25.50, 2));
-        order1.AddProduct(new Product("USB cord", "A1014", 95.00, 1));
+        order1.AddProduct(new Product("Wireless Keyboard", "P3001", 59.99, 1));
+        order1.AddProduct(new Product("Bluetooth Mouse", "P3002", 34.95, 2));
+        order1.AddProduct(new Product("27-inch Monitor", "P3003", 249.99, 1));
 
-        // Order 2 - International Customer
-        Address address2 = new Address("121 King's St", "Toronto", "ON", "Canada");
-        Customer customer2 = new Customer("Matt Gerald", address2);
+        // Order 2
+        Address address2 = new Address("221 Baker Street", "London", "England", "United Kingdom");
+        Customer customer2 = new Customer("James Walker", address2);
         Order order2 = new Order(customer2);
-        order2.AddProduct(new Product("Battery", "A2011", 29.99, 1));
-        order2.AddProduct(new Product("Memory Chip", "A2012", 69.99, 1));
+        order2.AddProduct(new Product("Noise Cancelling Headphones", "P4001", 189.99, 1));
+        order2.AddProduct(new Product("Portable SSD 1TB", "P4002", 129.50, 1));
 
-        // Display Order 1
+        // Display 1
         Console.WriteLine(" ORDER 1 ");
         Console.WriteLine("Packing Label:");
         Console.WriteLine(order1.GetPackingLabel());
@@ -29,7 +29,7 @@ class Program
         Console.WriteLine($"Total Price: ${order1.GetTotalPrice():F2}");
         Console.WriteLine();
 
-        // Display Order 2
+        // Display 2
         Console.WriteLine(" ORDER 2 ");
         Console.WriteLine("Packing Label:");
         Console.WriteLine(order2.GetPackingLabel());
