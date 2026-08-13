abbb206655c1451348fde73edff82dd62ced8a80
angietdev <tatiana.rincon.guio@gmail.com>
2026-08-13 12:36:26 -0500

    Eternal Quest Program

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager manager = new GoalManager();
+        manager.Start();
+
+        // Creativity: Added a streak system with bonus points for consecutive events.
     }
 }
